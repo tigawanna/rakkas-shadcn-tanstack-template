fa9a3474e900b460a80314053c4184e6ac3b7d93
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-02-03 11:01:36 +0300

    fromat

--- src/components/navigation/mini-settings/MiniSettings.tsx
+++ src/components/navigation/mini-settings/MiniSettings.tsx
@@ -1,9 +1,5 @@
 import { useState } from "react";
-import {
-  Avatar,
-  AvatarFallback,
-
-} from "@/components/shadcn/ui/avatar";
+import { Avatar, AvatarFallback } from "@/components/shadcn/ui/avatar";
 import { Button } from "@/components/shadcn/ui/button";
 import {
   DropdownMenu,
@@ -13,17 +9,14 @@ import {
 } from "@/components/shadcn/ui/dropdown-menu";
 import { ThemeToggle } from "./ThemeToggle";
 
-
 interface MiniSettingsModalProps {}
 
 export function MiniSettingsModal({}: MiniSettingsModalProps) {
-
-
   const [open, setOpen] = useState(false);
   return (
     <DropdownMenu modal open={open} onOpenChange={setOpen}>
       <DropdownMenuTrigger asChild>
-        <Button  className="relative h-7 w-7 rounded-full">
+        <Button className="relative h-7 w-7 rounded-full">
           <Avatar className="h-8 w-8">
             {/* <AvatarImage src={user?.avatar} alt="@shadcn" /> */}
             <AvatarFallback>{"OP"}</AvatarFallback>
@@ -34,14 +27,11 @@ export function MiniSettingsModal({}: MiniSettingsModalProps) {
       <DropdownMenuContent className="w-56 " align="end" forceMount>
         <DropdownMenuSeparator />
 
-
-
         <DropdownMenuSeparator />
         {/* theme toggle */}
         <ThemeToggle />
         <DropdownMenuSeparator />
         {/* logout button */}
-   
       </DropdownMenuContent>
     </DropdownMenu>
   );
