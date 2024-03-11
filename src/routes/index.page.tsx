6527d56a603b9882b6cdeeb3347d32425067b2cc
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-03-11 06:56:51 +0300

    cleanup

--- src/routes/index.page.tsx
+++ src/routes/index.page.tsx
@@ -1,30 +1,30 @@
 import { Button } from "@/components/shadcn/ui/button";
 import { Home } from "lucide-react";
-import { PageProps} from "rakkasjs";
+import { PageProps } from "rakkasjs";
 import { toast } from "sonner";
 
-
 export default function HomePage({}: PageProps) {
-return (
-  <main className="flex items-center justify-center w-full max-h-screen h-full gap-3">
-    <h2 className="text-3xl font-bold">Rakkasjs shadcn template</h2>
-    <Button
-      variant="outline"
-      onClick={() =>
-        toast("Event has been created", {
-          description: "Sunday, December 03, 2023 at 9:00 AM",
-          action: {
-            label: "Undo",
-            onClick: () => console.log("Undo"),
-          },
+  return (
+    <main className="flex items-center justify-center w-full max-h-screen h-full gap-3">
+      <h2 className="text-3xl font-bold">Rakkasjs shadcn template</h2>
+      <Button
+        variant="outline"
+        onClick={() => {
+          toast("Event has been created", {
+            description: "Sunday, December 03, 2023 at 9:00 AM",
+            type: "warning",
+            action: {
+              label: "Undo",
+              onClick: () => console.log("Undo"),
+            },
 
-          icon: <Home className="" />,
-          position:"top-left",
-        })
-      }
-    >
-      Show Toast
-    </Button>
-  </main>
-);
+            icon: <Home className="" />,
+            position: "top-left",
+          });
+        }}
+      >
+        Show Toast
+      </Button>
+    </main>
+  );
 }
