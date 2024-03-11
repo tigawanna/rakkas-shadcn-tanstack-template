63400d0e3e8b03740d58eaa51f4a368292d4157e
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-03-11 12:35:46 +0300

    split up main page

---
+++ src/routes/dashboard/index.page.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@/components/shadcn/ui/button";
+import { Home } from "lucide-react";
+import { PageProps } from "rakkasjs"
+import { toast } from "sonner";
+export default function Page({}:PageProps) {
+return (
+  <div className="w-full h-full  flex flex-col items-center justify-center">
+    <h2 className="text-3xl font-bold">Rakkasjs shadcn template</h2>
+    <Button
+      variant="outline"
+      onClick={() => {
+        toast.success("Event has been created", {
+          description: "Sunday, December 03, 2023 at 9:00 AM",
+
+          action: {
+            label: "Undo",
+            onClick: () => console.log("Undo"),
+          },
+
+          icon: <Home className="" />,
+        });
+      }}
+    >
+      Show Toast
+    </Button>
+  </div>
+);}
