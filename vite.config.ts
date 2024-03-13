b88c3286026ccad958f4cca209c92575ead0d713
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-03-13 19:10:26 +0300

    add tabstck query

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import rakkas from "rakkasjs/vite-plugin";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 import analyze from "rollup-plugin-analyzer";
+import { rakkasTanstackQuery } from "@/lib/plugin/vite-plugin";
 
 const env = loadEnv("", process.cwd(), "");
 Object.assign(process.env, env);
@@ -11,7 +12,7 @@ export default defineConfig({
   ssr: {
     external: ["@auth/core", "rakkasjs/node-adapter"],
   },
-  plugins: [tsconfigPaths(), react(), rakkas({}), analyze({
+  plugins: [tsconfigPaths(), react(), rakkas({}),rakkasTanstackQuery(), analyze({
     // highlight the modules with size > 60kb
     filter(moduleObject) {
       return moduleObject.size > 6000;
